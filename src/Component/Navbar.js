2bb0a0a89aa666e049be392aed9b769dd12eeaa9
uliadib12 <uliadib@gmail.com>
2022-04-14 21:28:02 +0700

    edit nav bar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,7 +1,16 @@
 function Navbar(props) {
     return (
-        <div className="bg-blue-600 p-3">
-            Navbar
+        <div className="bg-blue-600 p-3 text-white">
+            <div className="flex justify-around">
+                <div className="font-medium">Logo</div>
+                <div className="flex gap-4">
+                    <div>Overview</div>
+                    <div>Landing</div>
+                    <div>Page</div>
+                    <div>App</div>
+                </div>
+                <div className="">Sign in</div>
+            </div>
         </div>
     )
 }
